9bef24584a3580f4307b75051f611249e89026b1
Frank Mathew Sajan <145902617+frankmathewsajan@users.noreply.github.com>
2025-12-07 11:46:10 +0530

    fixed lottie animation type issue

--- types/lottie.d.ts
+++ types/lottie.d.ts
@@ -13,4 +13,17 @@ declare global {
   }
 }
 
+declare module "react/jsx-runtime" {
+  namespace JSX {
+    interface IntrinsicElements {
+      "dotlottie-player": React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
+        src: string;
+        autoplay?: boolean;
+        loop?: boolean;
+        speed?: string;
+      };
+    }
+  }
+}
+
 export {};
